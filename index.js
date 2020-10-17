685c730680e895cbc2313e56d85ff5317c3ad6e6
Rodrigo Fontenele Carvalho <rodrigo@carvalho.me>
2020-10-17 18:12:44 +0200

    New functions to create a container and generate a sas for a container

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const { BlobSASPermissions, BlobServiceClient, StorageSharedKeyCredential, generateBlobSASQueryParameters: generateBlobSas } = require('@azure/storage-blob');
+const { BlobSASPermissions, BlobServiceClient, ContainerSASPermissions, StorageSharedKeyCredential, generateBlobSASQueryParameters: generateBlobSas } = require('@azure/storage-blob');
 const { Readable } = require('stream');
 const moment = require('moment');
 
@@ -33,6 +33,37 @@ module.exports.BlobStorageAdapter = class {
             contentType
         }
         return `${this.url}${container}/${filename}?${generateBlobSas(signature, this.credential).toString()}`;
+    }
+
+    /**
+     * @param {string} container 
+     * @param {Number} validFor The number of time units this blob's shared access signature should be valid for
+     * @param {string} validForUnit MomentJS compatible unit of time (e.g. minute, hour, day, etc.)
+     * @param {string} permissions
+     */
+    generateContainerSas(container, validFor = 1, validForUnit = 'hour', permissions = 'c') {
+        const start = moment().subtract(5, 'minute');
+        const end = start.clone().add(validFor, validForUnit);
+
+        const signature = {
+            startsOn: start.toDate(),
+            expiresOn: end.toDate(),
+            permissions: ContainerSASPermissions.parse(permissions),
+            containerName: container
+        }
+        return `${this.url}${container}?${generateBlobSas(signature, this.credential).toString()}`;
+    }
+
+    /**
+     * @param {string} container The name of the container to be created
+     */
+    async createContainer(container) {
+        let containerClient = this.blobService.getContainerClient(container);
+        const exists = await containerClient.exists();
+        if (!exists) {
+            await this.blobService.createContainer(container);
+        }
+        return containerClient;
     }
 
     /**
